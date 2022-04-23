f607a0362314c42f1fb91ad00767e2f7c7c441d2
Beau <cbeauhilton@gmail.com>
2022-04-22 22:27:20 -0500

    R configs

--- lvim/config.lua
+++ lvim/config.lua
@@ -138,13 +138,9 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- }
 
 -- Additional Plugins
--- lvim.plugins = {
---     {"folke/tokyonight.nvim"},
---     {
---       "folke/trouble.nvim",
---       cmd = "TroubleToggle",
---     },
--- }
+lvim.plugins = {
+    {"jalvesaq/Nvim-R"},
+}
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 -- lvim.autocommands.custom_groups = {
